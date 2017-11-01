b8b53831fd9ec27f50cd76989c2b0b4c471ed10d
mariusjopen <mail@mariusjopen.com>
2017-11-01 11:20:52 +0100

    Added Taxonomy to all post types

--- functions.php
+++ functions.php
@@ -1,12 +1,16 @@
 <?php
 
+// VARIABLES
+
 $theme = wp_get_theme();
 $ver = $theme->get('Version');
 
+// ACTIONS
 
 remove_action( 'wp_head', 'wp_generator' );
 add_filter( 'show_admin_bar', '__return_false' );
 
+// JAVACSRIPT, CSS
 
 function add_theme_scripts(){
 	global $ver;
@@ -15,12 +19,14 @@ function add_theme_scripts(){
 }
 add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
 
+// MENU
 
 function register_my_menu() {
   register_nav_menu('header-menu',__( 'Header Menu' ));
 }
 add_action( 'init', 'register_my_menu' );
 
+// ENSEMBLE
 
 function create_post_type_ensemble() {
   register_post_type( 'ensemble',
@@ -31,11 +37,27 @@ function create_post_type_ensemble() {
       ),
       'public' => true,
       'has_archive' => true,
+			'taxonomies'  => array( 'ensemble-category' ),
     )
   );
 }
 add_action( 'init', 'create_post_type_ensemble' );
 
+function create_post_type_category_ensemble() {
+
+    register_taxonomy(
+        'ensemble-category',
+        'ensemble',
+        array(
+            'label' => __( 'Category' ),
+            'rewrite' => array( 'slug' => 'ensemble-category' ),
+            'hierarchical' => true,
+        )
+    );
+}
+add_action( 'init', 'create_post_type_category_ensemble' );
+
+// STÜCKE
 
 function create_post_type_stuecke() {
   register_post_type( 'stuecke',
@@ -46,11 +68,27 @@ function create_post_type_stuecke() {
       ),
       'public' => true,
       'has_archive' => true,
+			'taxonomies'  => array( 'stuecke-category' ),
     )
   );
 }
 add_action( 'init', 'create_post_type_stuecke' );
 
+function create_post_type_category_stuecke() {
+
+    register_taxonomy(
+        'stuecke-category',
+        'stuecke',
+        array(
+            'label' => __( 'Category' ),
+            'rewrite' => array( 'slug' => 'stuecke-category' ),
+            'hierarchical' => true,
+        )
+    );
+}
+add_action( 'init', 'create_post_type_category_stuecke' );
+
+// AKTUELLES
 
 function create_post_type_aktuelles() {
   register_post_type( 'aktuelles',
@@ -61,10 +99,24 @@ function create_post_type_aktuelles() {
       ),
       'public' => true,
       'has_archive' => true,
+			'taxonomies'  => array( 'aktuelles-category' ),
     )
   );
 }
 add_action( 'init', 'create_post_type_aktuelles' );
 
+function create_post_type_category_aktuelles() {
+
+    register_taxonomy(
+        'aktuelles-category',
+        'aktuelles',
+        array(
+            'label' => __( 'Category' ),
+            'rewrite' => array( 'slug' => 'aktuelles-category' ),
+            'hierarchical' => true,
+        )
+    );
+}
+add_action( 'init', 'create_post_type_category_aktuelles' );
 
 ?>
