1084e11409e1eb1b5861571a44610c020923dd7e
mariusjopen <mail@mariusjopen.com>
2017-11-02 17:22:54 +0100

    page tabs adjusted

--- page.php
+++ page.php
@@ -42,15 +42,15 @@ if( have_rows('inhalt') ):
           if( $images ):
           ?>
             <ul>
-                <?php
-                foreach( $images as $image ):
-                ?>
-                    <li>
-                      <?php echo wp_get_attachment_image( $image['ID'], $size ); ?>
-                    </li>
-                <?php
-                endforeach;
-                ?>
+              <?php
+              foreach( $images as $image ):
+              ?>
+                <li>
+                  <?php echo wp_get_attachment_image( $image['ID'], $size ); ?>
+                </li>
+              <?php
+              endforeach;
+              ?>
             </ul>
           <?php
           endif;
