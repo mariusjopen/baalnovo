043551b75c63ca3ab7872fb878c638f5919b05f9
mariusjopen <mail@mariusjopen.com>
2017-11-03 00:51:22 +0100

    page template group php

--- page.php
+++ page.php
@@ -2,157 +2,165 @@
 
 <p>SEITE</p>
 
-<p>Vorschaubild</p>
+<div class="main-image">
+  <?php
+  $image = get_field('vorschau_bild');
+  $size = '_768';
+  if( $image ) {
+    echo wp_get_attachment_image( $image, $size );
+  }
+  ?>
+</div>
 
-<?php
-$image = get_field('vorschau_bild');
-$size = '_768';
-if( $image ) {
-  echo wp_get_attachment_image( $image, $size );
-}
-?>
+<div class="group-container">
+  <?php
+  if( have_rows('inhalt') ):
 
-<p>Gruppe</p>
+    while ( have_rows('inhalt') ) : the_row();
 
-<?php
-if( have_rows('inhalt') ):
-
-  while ( have_rows('inhalt') ) : the_row();
-
-    if( get_row_layout() == 'group_text' ):
-    ?>
-
-    	<p><?php echo get_sub_field('text'); ?></p>
-
-    <?php
-    elseif( get_row_layout() == 'group_bild' ):
-    ?>
-
-      <?php
-      $image = get_sub_field('bild');
-      $size = '_768';
-      if( $image ) {
-        echo wp_get_attachment_image( $image, $size );
-      }
+      if( get_row_layout() == 'group_text' ):
       ?>
 
-    <?php
-    elseif( get_row_layout() == 'group_galerie' ):
-    ?>
+        <div class="group-text">
+      	   <p><?php echo get_sub_field('text'); ?></p>
+        </div>
 
       <?php
-      $images = get_sub_field('galerie');
-      $size = '_768';
-      if( $images ):
+      elseif( get_row_layout() == 'group_bild' ):
       ?>
-        <ul>
+
+        <div class="group-bild">
           <?php
-          foreach( $images as $image ):
+          $image = get_sub_field('bild');
+          $size = '_768';
+          if( $image ) {
+            echo wp_get_attachment_image( $image, $size );
+          }
           ?>
-            <li>
-              <?php echo wp_get_attachment_image( $image['ID'], $size ); ?>
-            </li>
+        </div>
+
+      <?php
+      elseif( get_row_layout() == 'group_galerie' ):
+      ?>
+
+        <div class="group-galerie">
           <?php
-          endforeach;
+          $images = get_sub_field('galerie');
+          $size = '_768';
+          if( $images ):
           ?>
-        </ul>
-      <?php
-      endif;
-      ?>
-
-    <?php
-    elseif( get_row_layout() == 'group_media' ):
-    ?>
-
-      <div class="embed-container">
-        <?php
-        $iframe = get_sub_field('media');
-
-        preg_match('/src="(.+?)"/', $iframe, $matches);
-        $src = $matches[1];
-
-        $params = array(
-          'controls'    => 0,
-          'hd'        => 1,
-          'autohide'    => 1
-        );
-
-        $new_src = add_query_arg($params, $src);
-        $iframe = str_replace($src, $new_src, $iframe);
-        $attributes = 'frameborder="0"';
-        $iframe = str_replace('></iframe>', ' ' . $attributes . '></iframe>', $iframe);
-
-        echo $iframe;
-        ?>
-      </div>
+            <ul>
+              <?php
+              foreach( $images as $image ):
+              ?>
+                <li>
+                  <?php echo wp_get_attachment_image( $image['ID'], $size ); ?>
+                </li>
+              <?php
+              endforeach;
+              ?>
+            </ul>
+          <?php
+          endif;
+          ?>
+        </div>
 
       <?php
-      elseif( get_row_layout() == 'group_stuck' ):
+      elseif( get_row_layout() == 'group_media' ):
       ?>
 
+        <div class="group-embed">
+          <?php
+          $iframe = get_sub_field('media');
+
+          preg_match('/src="(.+?)"/', $iframe, $matches);
+          $src = $matches[1];
+
+          $params = array(
+            'controls'    => 0,
+            'hd'        => 1,
+            'autohide'    => 1
+          );
+
+          $new_src = add_query_arg($params, $src);
+          $iframe = str_replace($src, $new_src, $iframe);
+          $attributes = 'frameborder="0"';
+          $iframe = str_replace('></iframe>', ' ' . $attributes . '></iframe>', $iframe);
+
+          echo $iframe;
+          ?>
+        </div>
+
         <?php
-        echo get_sub_field('stucke_einfugen');
-        $post_objects = get_sub_field('stucke_einfugen');
-        if( $post_objects ):
+        elseif( get_row_layout() == 'group_stuck' ):
         ?>
 
-          <ul>
-            <?php foreach( $post_objects as $post): ?>
-              <?php setup_postdata($post); ?>
-              <li>
-                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-              </li>
-            <?php endforeach; ?>
-          </ul>
+          <div class="group-stuck">
+            <?php
+            $post_objects = get_sub_field('stucke_einfugen');
+            if( $post_objects ):
+            ?>
 
-          <?php wp_reset_postdata(); ?>
+              <ul>
+                <?php foreach( $post_objects as $post): ?>
+                  <?php setup_postdata($post); ?>
+                  <li>
+                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                  </li>
+                <?php endforeach; ?>
+              </ul>
+
+              <?php wp_reset_postdata(); ?>
+
+            <?php
+            endif;
+            ?>
+          </div>
+
+        <?php
+        elseif( get_row_layout() == 'group_aktuelles' ):
+        ?>
+
+          <div class="group-aktuelles">
+            <?php
+            $post_objects = get_sub_field('aktuelles_einfugen');
+            if( $post_objects ):
+            ?>
+
+              <ul>
+                <?php foreach( $post_objects as $post): ?>
+                  <?php setup_postdata($post); ?>
+                  <li>
+                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+
+                    <?php
+                    $image = the_sub_field('vorschau_bild', $post_object->ID);
+                    $size = '_768';
+                    if( $image ) {
+                      echo wp_get_attachment_image( $image, $size );
+                    }
+                    ?>
+                  </li>
+                <?php endforeach; ?>
+              </ul>
+
+              <?php wp_reset_postdata(); ?>
+
+            <?php
+            endif;
+            ?>
+          </div>
 
         <?php
         endif;
-        ?>
 
-      <?php
-      elseif( get_row_layout() == 'group_aktuelles' ):
-      ?>
+      endwhile;
 
-        <?php
-        $post_objects = get_sub_field('aktuelles_einfugen');
-        if( $post_objects ):
-        ?>
+    else :
 
-          <ul>
-            <?php foreach( $post_objects as $post): ?>
-              <?php setup_postdata($post); ?>
-              <li>
-                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-
-                <?php
-                $image = the_sub_field('vorschau_bild', $post_object->ID);
-                $size = '_768';
-                if( $image ) {
-                  echo wp_get_attachment_image( $image, $size );
-                }
-                ?>
-              </li>
-            <?php endforeach; ?>
-          </ul>
-
-          <?php wp_reset_postdata(); ?>
-
-        <?php
-        endif;
-        ?>
-
-      <?php
-      endif;
-
-    endwhile;
-
-  else :
-
-endif;
-?>
-
+  endif;
+  ?>
+</div>
 
 
 
