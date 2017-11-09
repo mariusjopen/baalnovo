374869c93a322909ee20966accd2f8aac3721afb
mariusjopen <mail@mariusjopen.com>
2017-11-09 16:59:22 +0100

    pass event fornt page variables to next page

--- front-page.php
+++ front-page.php
@@ -44,11 +44,10 @@
 
             $date_time = get_sub_field('date', false, false) . get_sub_field('zeit', false, false);
             $date_time_mod = str_replace(':', '', $date_time);
-
             ?>
 
-            <div class="post-event" data-date="<?php echo $date_time_mod ?>">
-              <a href="<?php the_permalink() ?>">
+            <div class="post-event" data-date="">
+              <a href="<?php the_permalink() ?>?date_time=<?php echo $date_time_mod ?>">
                 <div class="title"><?php the_title(); ?></div>
                 <div class="date"><?php echo get_sub_field('date'); ?></div>
                 <div class="time"><?php echo get_sub_field('zeit'); ?></div>
