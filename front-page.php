b9aa863c392ccebcff666fc3352acdbc6cc42cb1
mariusjopen <mail@mariusjopen.com>
2017-11-09 16:51:24 +0100

    replace : in datr anf time in PHP and not jquery

--- front-page.php
+++ front-page.php
@@ -41,15 +41,23 @@
           <?php
           if( have_rows('events') ):
             while( have_rows('events') ): the_row();
+
+            $date_time = get_sub_field('date', false, false) . get_sub_field('zeit', false, false);
+            $date_time_mod = str_replace(':', '', $date_time);
+
             ?>
-              <div class="post-event" data-date="<?php echo get_sub_field('date', false, false);echo get_sub_field('zeit', false, false); ?>">
+
+            <div class="post-event" data-date="<?php echo $date_time_mod ?>">
+              <a href="<?php the_permalink() ?>">
                 <div class="title"><?php the_title(); ?></div>
                 <div class="date"><?php echo get_sub_field('date'); ?></div>
                 <div class="time"><?php echo get_sub_field('zeit'); ?></div>
                 <div class="location"><?php echo get_sub_field('ort'); ?></div>
                 <div class="city"><?php echo get_sub_field('stadt'); ?></div>
-                </br>
-              </div>
+              </br>
+              </a>
+            </div>
+
 
             <?php
             endwhile;
--- js/date-sorting.js
+++ js/date-sorting.js
@@ -2,17 +2,12 @@ jQuery( document ).ready(function() {
 
    var sorted = Array.prototype.sort.call(jQuery(".post-event"), function(a,b){
 
-     var date_a = jQuery(a).data("date").replace(':', '');
-     var date_b = jQuery(b).data("date").replace(':', '');
+     var date_a = jQuery(a).data("date");
+     var date_b = jQuery(b).data("date");
 
-     var new_date_a = parseInt(date_a,14);
-     var new_date_b = parseInt(date_b,14);
-
-     return new Date(new_date_a) < new Date(new_date_b) ;
+     return new Date(date_a) < new Date(date_b) ;
 
    });
-
-
 
   jQuery(sorted).each(function(){
     jQuery(".spielplan").prepend(this);
