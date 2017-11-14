ea56036293b8a7220f9095f50bc58fb221532445
mariusjopen <mail@mariusjopen.com>
2017-11-14 16:18:30 +0100

    ensemble liste done

--- page-ensemble.php
+++ page-ensemble.php
@@ -16,9 +16,85 @@
 
 <div class="content">
 
+  	<?php
+  	$post_type = 'ensemble';
+  	$taxonomies = get_object_taxonomies( array( 'post_type' => $post_type ) );
+
+    foreach( $taxonomies as $taxonomy ) :
+      $terms = get_terms( $taxonomy );
+      foreach( $terms as $term ) :
+      ?>
+
+          <div class="category-section">
+
+  	        <div class="kategorie-name">
+  	            <p><?php echo $term->name; ?></p>
+  	        </div>
+
+  	        <?php
+  	        $args = array(
+              'post_type' => $post_type,
+              'posts_per_page' => -1,
+        			'orderby' => 'title',
+              'order' => 'ASC',
+              'tax_query' => array(
+                array(
+                  'taxonomy' => $taxonomy,
+                  'field' => 'slug',
+                  'terms' => $term->slug
+                )
+              )
+            );
+
+  	        $posts = new WP_Query($args);
+
+  	        if( $posts->have_posts() ):
+              while( $posts->have_posts() ) :
+                $posts->the_post();
+                ?>
+
+          				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+                    <div class="vorschau">
+                      <?php
+                      if( have_rows('bilder') ):
+                        while( have_rows('bilder') ): the_row();
+
+                          $image = get_sub_field('portrait');
+                          $size = '_768';
+                          if( $image ) {
+                            echo wp_get_attachment_image( $image, $size );
+                          }
+                          ?>
+
+                          <p><?php echo get_sub_field('kurzer_text'); ?></p>
+
+                        <?php
+                        endwhile;
+                      endif;
+                      ?>
+                    </div>
+
+                    <a href="<?php the_permalink() ?>">
+                      <?php the_title(); ?>
+                    </a>
+
+          				</div>
+
+            	<?php
+              endwhile;
+            endif;
+            ?>
+
+          </div>
+
+      <?php
+      endforeach;
+    endforeach;
+    ?>
 
 
-  
+
 </div>
 
 
