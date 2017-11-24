ffb472a8e55639928c1a0cf04b1547649d03e7b5
mariusjopen <mail@mariusjopen.com>
2017-11-24 16:51:06 +0100

    images css

--- inc/image-poster.php
+++ inc/image-poster.php
@@ -1,10 +1,12 @@
 <!-- START IMAGE POSTER -->
 <div class="image-poster">
+	<div class="inside">
   <?php
   $size = '_768';
   if( $image ) {
     echo wp_get_attachment_image( $image, $size );
   }
   ?>
+	</div>
 </div>
 <!-- END IMAGE POSTER -->
