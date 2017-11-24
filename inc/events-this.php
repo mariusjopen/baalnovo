2773edc91df564839ba2d16745821183e2a743de
mariusjopen <mail@mariusjopen.com>
2017-11-24 16:43:30 +0100

    events stuecke styled

--- inc/events-this.php
+++ inc/events-this.php
@@ -10,16 +10,18 @@
 		if ($date_url == $date_time_mod) {
 		?>
 
-			<div class="ticket"><a href="<?php echo get_sub_field('ticket'); ?>" target="_blank" >Ticket</a></div>
+			<div class="ticket"><a href="<?php echo get_sub_field('ticket'); ?>" target="_blank" >Ticket Kaufen</a></div>
 
 			<div class="post-event" data-date="<?php echo $date_time_mod ?>">
-				<a href="<?php the_permalink() ?>?date_time=<?php echo $date_time_mod ?>">
+				<!-- <a href="<?php the_permalink() ?>?date_time=<?php echo $date_time_mod ?>"> -->
 					<div class="title"><?php the_title(); ?></div>
-					<div class="date"><?php echo get_sub_field('date'); ?></div>
-					<div class="time"><?php echo get_sub_field('zeit'); ?></div>
-					<div class="location"><?php echo get_sub_field('ort'); ?></div>
-					<div class="city"><?php echo get_sub_field('stadt'); ?></div>
-				</a>
+					<div class="date-time">
+						<?php echo get_sub_field('date'); ?> | <?php echo get_sub_field('zeit'); ?>
+					</div>
+					<div class="location-city">
+						<div class="location"><?php echo get_sub_field('ort'); ?>, <?php echo get_sub_field('stadt'); ?></div>
+					</div>
+				<!-- </a> -->
 			</div>
 
 		<?php
