0af421b18c2c0ab9d8a98f34d0ea851558b0bff8
mariusjopen <mail@mariusjopen.com>
2017-11-30 13:25:07 +0800

    single stück event in url

--- inc/events-this.php
+++ inc/events-this.php
@@ -1,44 +1,56 @@
 <!-- START EVENTS THIS -->
-<?php if( have_rows($event) ): ?>
-	<div class="events-this">
+<?php
+if( have_rows($event) ):
 
-		<div class="element-title">
-			Dieses Event
-		</div>
+	if ($date_url != "") :
+	?>
 
-		<?php
-		while( have_rows($event) ): the_row();
+		<div class="events-this">
 
-		$date_time = get_sub_field('date', false, false) . get_sub_field('zeit', false, false);
-		$date_time_mod = str_replace(':', '', $date_time);
-
-		if ($date_url == $date_time_mod) {
-		?>
-
-
-			<div class="post-event" data-date="<?php echo $date_time_mod ?>">
-				<!-- <a href="<?php the_permalink() ?>?date_time=<?php echo $date_time_mod ?>"> -->
-					<div class="title"><?php the_title(); ?></div>
-					<div class="date-time">
-						<?php echo get_sub_field('date'); ?> | <?php echo get_sub_field('zeit'); ?>
-					</div>
-					<div class="location-city">
-						<div class="location"><?php echo get_sub_field('ort'); ?>, <?php echo get_sub_field('stadt'); ?></div>
-					</div>
-				<!-- </a> -->
+			<div class="element-title">
+				Dieses Event
 			</div>
 
-			<div class="ticket"><a href="<?php echo get_sub_field('ticket'); ?>" target="_blank" >Ticket Kaufen</a></div>
+			<?php
+			while( have_rows($event) ): the_row();
+
+			$date_time = get_sub_field('date', false, false) . get_sub_field('zeit', false, false);
+			$date_time_mod = str_replace(':', '', $date_time);
+
+			if ($date_url == $date_time_mod) {
+			?>
 
 
-		<?php
-		};
-		?>
+				<div class="post-event" data-date="<?php echo $date_time_mod ?>">
+					<!-- <a href="<?php the_permalink() ?>?date_time=<?php echo $date_time_mod ?>"> -->
+						<div class="title"><?php the_title(); ?></div>
+						<div class="date-time">
+							<?php echo get_sub_field('date'); ?> | <?php echo get_sub_field('zeit'); ?>
+						</div>
+						<div class="location-city">
+							<div class="location"><?php echo get_sub_field('ort'); ?>, <?php echo get_sub_field('stadt'); ?></div>
+						</div>
+					<!-- </a> -->
+				</div>
 
-		<?php
-		endwhile;
-		?>
-	</div>
+				<div class="ticket"><a href="<?php echo get_sub_field('ticket'); ?>" target="_blank" >Ticket Kaufen</a></div>
 
-<?php endif; ?>
+
+			<?php
+			};
+
+
+
+			?>
+
+			<?php
+			endwhile;
+			?>
+		</div>
+
+	<?php
+	endif;
+
+endif;
+?>
 <!-- END EVENTS THIS -->
