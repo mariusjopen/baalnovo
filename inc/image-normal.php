eecf29983dbb61181332de549292990d3e53fe0d
mariusjopen <mail@mariusjopen.com>
2017-12-01 20:18:35 +0800

    captions

--- inc/image-normal.php
+++ inc/image-normal.php
@@ -6,8 +6,22 @@
 		  <?php
 		  $size = '_768';
 		  echo wp_get_attachment_image( $image, $size );
-		  ?>
+			?>
 		</div>
+
+		<?php
+		$image_array =  wp_get_attachment($image  );
+		$image_array_caption = $image_array["caption"];
+
+		if( $image_array_caption ):
+		?>
+
+			<div class="caption">
+				<?php echo $image_array_caption; ?>
+			</div>
+
+		<?php endif; ?>
+
 	</div>
 
 <?php endif; ?>
