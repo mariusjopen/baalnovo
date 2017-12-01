bfc56b889285f1ebc30b59c8ca2fc3004195e245
mariusjopen <mail@mariusjopen.com>
2017-12-01 20:32:46 +0800

    posert have links now

--- inc/vorschau-stuecke.php
+++ inc/vorschau-stuecke.php
@@ -1,8 +1,12 @@
 <!-- START VORSCHAU STUECKE -->
-<?php
-$image = get_field('poster');
-include(locate_template('inc/image-poster.php'));
+<a href="<?php the_permalink() ?>">
+	<?php
+	$image = get_field('poster');
+	include(locate_template('inc/image-poster.php'));
+	?>
+</a>
 
+<?php
 include(locate_template('inc/title-link.php'));
 ?>
 <!-- END VORSCHAU STUECKE -->
